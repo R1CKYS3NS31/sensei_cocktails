e355445c8279fa6dcba421d27aa931a574e0fa21
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2021-12-21 11:30:29 +0300

    handled searching of cocktails functionalities

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+<h1 className="section-title">about us</h1>
+<p>
+  We sell cocktails, conctact us @+254712345678
+</p>
+    </section>
   )
 }
 
